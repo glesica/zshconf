af91e9b63ee9805efefa8ede0d22f0b046630d62
George Lesica <george@lesica.com>
2020-07-06 16:10:02 -0600

    Update installer to install OMZ

--- install.sh
+++ install.sh
@@ -14,8 +14,32 @@ ln -s "$PWD/init.vim" "$HOME/.config/nvim/init.vim" || echo "init.vim already ex
 # Install Oh My Zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
+# Install spaceship theme
+git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
+
+# A helper to create symlinks to the config files without blowing away anything
+# important (hopefully).
+function install_file () {
+    local FILENAME="$1"
+    local DSTPATH="$HOME/.$FILENAME"
+    local SRCPATH="$PWD/$FILENAME"
+
+    if [ -L "$DSTPATH" ]; then
+        echo "deleting link $DSTPATH"
+        rm -f "$DSTPATH"
+    fi
+
+    if [ -e "$FILENAME" ]; then
+        echo "backing up $DSTPATH"
+        mv "$DSTPATH" "$DSTPATH.`date`"
+    fi
+
+    ln -s "$SRCPATH" "$DSTPATH" || echo "failed to link $FILENAME"
+}
+
 # Symlink zsh configuration
-ln -s "$PWD/zshrc" "$HOME/.zshrc" || echo ".zshrc already exists"
-ln -s "$PWD/zshalias" "$HOME/.zshalias" || echo ".zshalias already exists"
-ln -s "$PWD/zshpath" "$HOME/.zshpath" || echo ".zshpath already exists"
-ln -s "$PWD/profile" "$HOME/.profile" || echo ".profile already exists"
+install_file zshrc
+install_file zshalias
+install_file zshpath
+install_file profile
+
