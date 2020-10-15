d4d929364e0ea832035f0186b85f6e7b48fb1da8
George Lesica <george@lesica.com>
2020-10-15 09:47:07 -0600

    Make installer more robust

--- install.sh
+++ install.sh
@@ -2,35 +2,40 @@
 
 set -e
 
+ZSH_CUSTOM="$HOME/.oh-my-zsh/custom"
+
 # Install vim-plug (from master because I'm reckless)
 curl -fLo \
     "$HOME/.config/nvim/autoload/plug.vim" \
     --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-# Symlink init.vim
-ln -s "$PWD/init.vim" "$HOME/.config/nvim/init.vim" || echo "init.vim already exists"
-
 # Install Oh My Zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended || echo "oh my zsh already installed"
 
 # Install spaceship theme
-git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
+if [ ! -d "$ZSH_CUSTOM/themes/spaceship-prompt" ]; then
+    git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
+fi
 
 # A helper to create symlinks to the config files without blowing away anything
 # important (hopefully).
 function install_file () {
     local FILENAME="$1"
-    local DSTPATH="$HOME/.$FILENAME"
+    local DESTINATION="$2"
+    local PREFIX="$3"
+    local DSTPATH="$DESTINATION/$PREFIX$FILENAME"
     local SRCPATH="$PWD/$FILENAME"
 
+    echo "Installing $FILENAME"
+
     if [ -L "$DSTPATH" ]; then
-        echo "deleting link $DSTPATH"
+        echo "Deleting link $DSTPATH"
         rm -f "$DSTPATH"
     fi
 
-    if [ -e "$FILENAME" ]; then
-        echo "backing up $DSTPATH"
+    if [ -e "$DSTPATH" ]; then
+        echo "Backing up $DSTPATH"
         mv "$DSTPATH" "$DSTPATH.`date`"
     fi
 
@@ -38,8 +43,9 @@ function install_file () {
 }
 
 # Symlink zsh configuration
-install_file zshrc
-install_file zshalias
-install_file zshpath
-install_file profile
+install_file zshrc "$HOME" "."
+install_file zshalias "$HOME" "."
+install_file zshpath "$HOME" "."
+install_file profile "$HOME" "."
+install_file init.vim "$HOME/.config/nvim" ""
 
